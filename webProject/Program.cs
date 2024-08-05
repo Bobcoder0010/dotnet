feb7ca9a5810427fee2db6c76a8ac9c23bc15037
Bobcoder0010 <dellizulter@gmail.com>
2024-08-04 20:21:03 -0500

    Ado dot net

---
+++ webProject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace AdoNetProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Nepal";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string sql = "SELECT * FROM YourTable";
+                SqlCommand command = new SqlCommand(sql, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader[0]} {reader[1]}");
+                }
+            }
+        }
+    }
+}
